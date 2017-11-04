868c42e8aa53d23a6a7ff5f7188cc9f17f0b9ee2
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-11-04 15:39:41 -0700

    Added login to navbar. Added 'start' to header.

--- audiotours/src/app/header/index.js
+++ audiotours/src/app/header/index.js
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       <h1 className="header__text">Never travel without a guide.</h1>
+      <span className="header__button"><span className="header__button__text">Start Your Adventure</span></span>
       <p className="header__image__caption">
         Photo by <a href="https://unsplash.com/@dariuszsankowski">Dariusz Sankowski</a> on
         <a href="https://unsplash.com/"> Unsplash</a>
--- audiotours/src/app/navigation/nav.js
+++ audiotours/src/app/navigation/nav.js
@@ -8,6 +8,11 @@ const MainNav = () => {
       <a className="main_nav__link" href="#">ABOUT</a>
       <a className="main_nav__link" href="#">APP</a>
       <a className="main_nav__link" href="#">CONTACT</a>
+      
+      <div className="main_nav__login_box">
+        <a className="main_nav__link login" href="#">Login</a>
+        <a className="main_nav__link register" href="#">Register</a>
+      </div>
     </nav>
   );
 };
