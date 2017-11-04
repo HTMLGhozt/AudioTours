94003b955a5380c47edcc6f75b1eff774615a1d8
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-11-04 16:32:11 -0700

    removed header button

--- audiotours/src/app/header/index.js
+++ audiotours/src/app/header/index.js
@@ -6,6 +6,7 @@ const Header = () => {
     <div className="header">
       <h1 className="header__text">Never travel without a guide.</h1>
       <span className="header__button"><span className="header__button__text">Start Your Adventure</span></span>
+
       <p className="header__image__caption">
         Photo by <a href="https://unsplash.com/@dariuszsankowski">Dariusz Sankowski</a> on
         <a href="https://unsplash.com/"> Unsplash</a>
