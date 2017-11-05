042e3a20197ae6f7529e88257c7e45c19f234ef8
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-11-05 15:35:06 -0700

    fleshed out section 1, added white space for the next section.

--- audiotours/src/app/sections/Sections-Container.js
+++ audiotours/src/app/sections/Sections-Container.js
@@ -4,14 +4,23 @@ import './sections.css';
 const SectionsContainer = () => {
   return (
     <div className="sections_wrapper">
+
       <div className="section_container">
         <div className="section_container__column user">
-          <span className="section_container__title">Listen</span>
+          <div className="section_container__text">
+            <span className="section_container__title">Listen</span>
+          </div>
+          <div className="section_container__icons">
+            <div></div>
+            <div></div>
+          </div>
         </div>
         <div className="section_container__column create">
           <span className="section_container__title">Create</span>
         </div>
-      </div>    
+      </div>
+      <div className="section_container__2">
+      </div>
     </div>
   );
 };
